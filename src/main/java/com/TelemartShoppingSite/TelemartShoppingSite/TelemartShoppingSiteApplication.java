ad167b8cf85d987ee3f7d27378984f0b011c480f
NitinBhattacharyya <42495409+NitinBhattacharyya@users.noreply.github.com>
2023-11-17 21:31:15 +0530

    First

---
+++ src/main/java/com/TelemartShoppingSite/TelemartShoppingSite/TelemartShoppingSiteApplication.java
@@ -0,0 +1,13 @@
+package com.TelemartShoppingSite.TelemartShoppingSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelemartShoppingSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelemartShoppingSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TelemartShoppingSite/TelemartShoppingSite/TelemartShoppingSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TelemartShoppingSite.TelemartShoppingSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelemartShoppingSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
